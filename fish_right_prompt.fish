d7f57a39383e51eef73b21f13ba440d19f5f4345
l4zygreed <l4zygreed@gmail.com>
2022-10-31 10:49:09 +0200

    Initial commit

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,10 +1,10 @@
 function fish_right_prompt -d "Write out the right prompt of the lazy theme"
-  set -l colnormal (set_color normal)
-  set -l ps_where $colnormal(whoami)@(hostname|cut -d . -f 1)
+  # set -l colnormal (set_color normal)
+  # set -l ps_where $colnormal(whoami)@(hostname|cut -d . -f 1)
   
-  # Right Prompt
+  # # Right Prompt
 
-  if test $__lazy_display_rprompt -eq 1
-    echo -n -s $ps_where
-  end
+  # if test $__lazy_display_rprompt -eq 1
+  #   echo -n -s $ps_where
+  # end
 end
